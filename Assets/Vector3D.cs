70cc83b857f55e61be2cd713e6aaacff55cea1bf
Waldemar Just <Waldemarjust@gmx.de>
2016-11-16 14:06:48 +0100

    [Updated] Vector3D

---
+++ Assets/Vector3D.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vector3D : MonoBehaviour
+{
+
+
+    public float x, y, z;
+    public Vector3D(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public Vector3D Translate(Vector3D end)
+    {
+        return new Vector3D(end.x, end.y, end.z);
+    }
+
+    public static implicit operator Vector3(Vector3D m)
+    {
+        return new Vector3(m.x, m.y, m.z);
+    }
+
+    public static implicit operator Vector3D(Vector3 m)
+    {
+        return new Vector3D(m.x, m.y, m.z);
+    }
+
+    public Vector3D Position(GameObject A)
+    {
+        return new Vector3D(A.transform.position.x, A.transform.position.y, A.transform.position.z);
+    }
+
+    public static Vector3D Falling (GameObject B, float Fallingspeed)
+    {
+        return new Vector3D(B.transform.position.x, B.transform.position.y - Fallingspeed, B.transform.position.z);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
